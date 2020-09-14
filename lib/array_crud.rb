c8a865fcab70cd3e1efd0cdb2569dac81b0d6fda
Terrell Murrell <littlerell12@gmail.com>
2020-09-14 21:49:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  new_Array = []
+  empty_Array = []
 end
 
 def create_an_array
-
+  four_Array = ["Dog", "Cat", "Pig", "Mouse"]
 end
 
 def add_element_to_end_of_array(array, element)
